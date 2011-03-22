802b16005f688637af6019dc12e79bf171b5fa8b
rajkumar47 <rajkumar.rajendran.work@gmail.com>
2011-03-22 12:28:31 -0400

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -9,5 +9,7 @@ end
 
 group :test do
  gem 'rspec', '2.5.0'
+ gem 'webrat'
+ gem 'spork'
 end
 	
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,19 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
-RSpec.configure do |config|
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -24,4 +30,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  end
+end
+
+Spork.each_run do
 end
